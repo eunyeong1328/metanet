8e402c3a18dadb3fce284432cd8c42bbaca525b9
eunyoung56 <eunyeong1328@daum.net>
2023-02-06 10:56:52 +0900

    confirm lists by subject

--- src/kosa/school/Course.java
+++ src/kosa/school/Course.java
@@ -1,29 +1,45 @@
 package kosa.school;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Course {
 	private String name;
-	private String content;
+	private List<Student> students;
 	
 	public Course() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-	public Course(String name, String content) {
+	public Course(String name) {
 		super();
 		this.name = name;
-		this.content = content;
+		students = new ArrayList<Student>();
 	}
+	
+	//학생 추가
+	public void addStudent(Student student) {
+		students.add(student);
+	}
+	
+	//학생 삭제
+	public void removeStudent(Student student) {
+		students.remove(student);
+	}
+	
+	//print
+	public void printCourse() {
+		System.out.println("과목명 : " + name);
+		for(Student student : students) {
+			System.out.println("수강과목 : " + student.getName());
+		}
+	}
+	
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
+	
 
 }
--- src/kosa/school/Student.java
+++ src/kosa/school/Student.java
@@ -7,29 +7,40 @@ public class Student {
 	private String name;
 	private List<Course> courseList;//학생이 수강신청한 과목들
 	
-	
-	public Student() {
-	}
+	public Student() {}
+	 
 	public Student(String name) {
 		super();
 		this.name = name;
 		courseList = new ArrayList<Course>();
 	}
 
-	//수강신청할 메소드가 필요하다
-	public void register(Course c) {
-		courseList.add(c);
+	//수강신청
+	public void register(Course course) {
+		courseList.add(course);
+		//과목별 학생들 추가
+		course.addStudent(this);
 	}
 	
+	//수강취소
+	public void dropCourse(Course course) {
+		if(courseList.contains(course)) {
+			courseList.remove(course);
+			course.removeStudent(this);
+		}
+	}
+	//print
 	public void printMemter() {
 		System.out.println(" ===================================  ");
 		System.out.println("학생이름 : " + name);
 		for(Course course : courseList) {
-			System.out.println("수상과목 : " + course.getName());
-			System.out.println("수상내용 : " + course.getContent());
+			System.out.println("수강과목 : " + course.getName());
 		}
 	}
 
+	public void listPrint() {
+		System.out.println(" name 확인 : " + name );
+	}
 	public String getName() {
 		return name;
 	}
@@ -45,10 +56,5 @@ public class Student {
 	public void setCourseList(List<Course> courseList) {
 		this.courseList = courseList;
 	}
-
-	
-
-
-	
 
 }
